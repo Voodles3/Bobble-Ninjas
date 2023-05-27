19870799941b542427563fdc0bd35e40f5a289a3
Voodles3 <voodles10@gmail.com>
2023-05-27 08:20:29 -0700

    Adding basic dashing
    Added simple dashing by pressing space. Very unbalanced for now and doesn't have a cooldown, I'll implement that next time. You can adjust dash power and movement speed in the unity editor under the Movement script attached to the player.

--- Bobble Ninjas/Assets/Scripts/Movement.cs
+++ Bobble Ninjas/Assets/Scripts/Movement.cs
@@ -10,7 +10,13 @@ public class Movement : MonoBehaviour
     float horizInput;
     float vertInput;
 
+    Vector3 direction;
+
     public float movementSpeed = 5f;
+    public float dashPower = 5f;
+
+    bool moving = false;
+    bool dashing = false;
 
     void Start()
     {
@@ -19,19 +25,59 @@ public class Movement : MonoBehaviour
 
     void Update()
     {
+        CheckMovePlayer();
+        CheckDash();
+    }
+
+    void FixedUpdate()
+    {
         MovePlayer();
+        Dash();
     }
 
     void MovePlayer()
     {
+        if (moving)
+        {
+            rb.AddRelativeForce(direction * movementSpeed * Time.deltaTime);
+        }
+    }
+
+    void Dash()
+    {
+        if (dashing)
+        {
+            rb.AddRelativeForce(direction * dashPower * Time.deltaTime, ForceMode.Impulse);
+            dashing = false;
+        }
+    }
+
+    void CheckMovePlayer()
+    {
         horizInput = Input.GetAxisRaw("Horizontal");
         vertInput = Input.GetAxisRaw("Vertical");
-
-        Vector3 direction = new Vector3(horizInput, 0f, vertInput).normalized;
+        direction = new Vector3(horizInput, 0f, vertInput).normalized;
 
         if(direction.magnitude >= 0.01f)
         {
-            rb.AddRelativeForce(direction * movementSpeed * Time.deltaTime);
+            moving = true;
+            
+        }
+        else
+        {
+            moving = false;
+        }
+    }
+
+    void CheckDash()
+    {
+        horizInput = Input.GetAxisRaw("Horizontal");
+        vertInput = Input.GetAxisRaw("Vertical");
+        direction = new Vector3(horizInput, 0f, vertInput).normalized;
+
+        if (Input.GetKeyDown(KeyCode.Space) && direction.magnitude >= 0.01f)
+        {
+            dashing = true;
         }
     }
 
